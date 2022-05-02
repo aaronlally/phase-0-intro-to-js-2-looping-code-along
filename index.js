135bf5454263dbbbd0902fec9abfd4a98bd45058
Aaron Lally <aaronblally@gmail.com>
2022-05-02 12:23:05 -0400

    commit tree 566ddb62abc07a139c17e4408af6137d09e9a9c0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function writeCards(stringNames, event) {
+    const cards = [];
+    for (let i = 0; i < stringNames.length; i++) {
+        cards.push(`Thank you, ${stringNames[i]}, for the wonderful ${event} gift!`); 
+        }
+        return cards;
+    }
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x);
+        x--;
+    } 
+}
